f3dff04b4fb2e6a9eba35376af5ceb3c02b52f4d
Hugo <hugohoffmann04@gmail.com>
2019-09-30 10:50:07 -0300

    chore: config global style

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import { Provider } from 'react-redux'
 import Routes from './routes'
 import store from './store'
+import GlobalStyle from './styles/global'
 
 const App = () => (
     <Provider store={store}>
-        <Routes/>
+        <Fragment>
+            <Routes/>
+            <GlobalStyle/>
+        </Fragment>
     </Provider>
 )
 
