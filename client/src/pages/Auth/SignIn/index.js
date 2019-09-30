25469bf9cf4c537542c286949e1d82c5055d0873
Hugo <hugohoffmann04@gmail.com>
2019-09-30 14:23:42 -0300

    new: create layout auth page

--- client/src/pages/Auth/SignIn/index.js
+++ client/src/pages/Auth/SignIn/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
+import Button from '~/styles/components/Button'
+import { Container, SignForm } from '../styles';
+
 const SignIn = () => {
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <SignForm onSubmit={ () => {} }>
+                <h1>Boas Vindas</h1>
+
+                <span>E-mail</span>
+                <input type="email" name="email"/>
+
+                <span>Senha</span>
+                <input type="password" name="password" />
+
+                <Button size="big" type="submit">
+                    Entrar
+                </Button>
+
+            </SignForm>
+        </Container>
     );
 }
 
