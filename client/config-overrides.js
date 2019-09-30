8f52dcdfd75f9b3258fbe06f160230a4e2523cd7
Hugo <hugohoffmann04@gmail.com>
2019-09-30 10:10:54 -0300

    chore: config routes

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,11 +1,10 @@
-const { injectBabelPlugin } = require('react-app-rewired')
+const { addBabelPlugin, override } = require('customize-cra'); 
 
-const rootImport = [
-    "root-import",
-    {
-        rootPathPrefix: "~",
-        rootPathSuffix: "src"
-    }
-]
-
-module.exports = config => injectBabelPlugin(rootImport, config)
+module.exports = override(
+    addBabelPlugin([
+        'babel-plugin-root-import',
+        {
+            rootPathSuffix: 'src', 
+        }
+    ])
+);
