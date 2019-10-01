74629979b33fe5b0fdf39ba2a51206ebe04f5602
Hugo <hugohoffmann04@gmail.com>
2019-10-01 13:38:27 -0300

    new: create projects layout

---
+++ client/src/components/Projects/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import Button from '~/styles/components/Button';
+import { Container, Project } from './styles';
+
+const Projects = ({ activeTeam }) => {
+    if(!activeTeam) return null;
+
+    return (<Container>
+        <header>
+            <h1>{activeTeam.name}</h1>
+            <div>
+                <Button onClick={ () => {} }>
+                    + Novo
+                </Button>
+                <Button onClick={ () => {} }>
+                    Membros
+                </Button>
+            </div>
+        </header>
+        <Project>
+            <p>Aplicação com alguma tecnologia</p>
+        </Project>
+        <Project>
+            <p>Aplicação com alguma tecnologia</p>
+        </Project>
+        <Project>
+            <p>Aplicação com alguma tecnologia</p>
+        </Project>
+        <Project>
+            <p>Aplicação com alguma tecnologia</p>
+        </Project>
+    </Container>)
+}
+
+Projects.propTypes = {
+    activeTeam: PropTypes.shape({
+        name: PropTypes.string,
+    }).isRequired,
+}
+
+const mapStateToProps = state => ({
+    activeTeam: state.teams.active
+});
+
+export default connect(mapStateToProps)(Projects);
--- client/src/pages/Main/index.js
+++ client/src/pages/Main/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import TeamSwitcher from '~/components/TeamSwitcher';
+import Projects from '~/components/Projects';
 
 import { Container } from './styles'
 const Main = () => {
     return (
         <Container>
             <TeamSwitcher/>
+            <Projects/>
         </Container>
     );
 }
