e853f593c29243260cdc1d2d3f8694a580e4963e
Hugo <hugohoffmann04@gmail.com>
2019-10-01 09:05:20 -0300

    new: layout teamSwitcher

--- client/src/pages/Main/index.js
+++ client/src/pages/Main/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import TeamSwitcher from '~/components/TeamSwitcher';
+
+import { Container } from './styles'
 const Main = () => {
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <TeamSwitcher/>
+        </Container>
     );
 }
 
